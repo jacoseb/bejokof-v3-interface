b7a8609b3eb2574884ce1554ee1813ad499be501
Jacoseb <jacobsebio1@gmail.com>
2025-09-08 00:21:45 -0500

    22. Load Balances

--- app/loans/page.js
+++ app/loans/page.js
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div className="page loans">
       <h1 className="title">Loans</h1>
-
     </div>
   );
 }
--- app/swap/page.js
+++ app/swap/page.js
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div className="page swap">
       <h1 className="title">Swap</h1>
-
     </div>
   );
 }
